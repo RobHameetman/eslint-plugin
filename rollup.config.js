bcc862e1529f78a7d1ed7a319e014b8201327607
Robert Hameetman <engineering@robhameetman.com>
2025-05-18 15:43:17 -0500

    fix: File enumerator error

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,7 @@ const config = (format = isModule ? 'esm' : 'cjs') => ({
 		/**
 		 * These remediate warnings throw by the resolve() plugin.
 		 */
-		/eslint\/lib\/cli-engine\/file-enumerator/,
+		// /eslint\/lib\/cli-engine\/file-enumerator/,
 		/eslint\/lib\/util\/glob-util/,
 	],
 	plugins: [
